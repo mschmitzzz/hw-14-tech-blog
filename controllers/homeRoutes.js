201e6fb4772e309b302f6989ac2d37302eb6d6ee
Mac Schmitz <mac.schmitz@att.net>
2022-06-24 10:24:15 -0400

    saved changes to homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -57,10 +57,10 @@ router.get('/', async (req, res) => {
   router.get('/dashboard', async (req, res) => {
   try {
     // Find the logged in user based on the session ID
-    // const userData = await User.findByPk(req.session.user_id, {
-    //   attributes: { exclude: ['password'] },
-    //   include: [{ model: Project }],
-    // });
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
 
     // const user = userData.get({ plain: true });
 
